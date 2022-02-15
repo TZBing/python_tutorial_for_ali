cab83aff994bf11bb69425ecd1d4b54b0913fc5c
YangJun <yj9525@163.com>
2022-02-15 18:33:45 +0800

    Add files via upload

---
+++ marquee_with_gui.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+
+class Marquee:
+    def __init__(self, root, marquee_str):
+        self.root = root
+        self.root.attributes('-topmost', True)
+        self.root.attributes('-fullscreen', True)
+        self.root.attributes('-transparentcolor', 'black')
+
+        self.str_var = tkinter.StringVar(value=marquee_str)
+
+        self.label = tkinter.Label(self.root)
+        self.label['textvariable'] = self.str_var
+        self.label['bg'] = 'black'
+        self.label['fg'] = '#d6604e'
+        self.label['font'] = ('TkHeadingFont', 75, 'bold')
+        self.label.pack(fill='both', expand=True)
+
+        self.interval = 150
+        self.root.after(self.interval, self.change_str)
+
+        self.root.mainloop()
+
+    def change_str(self):
+        tmp = self.str_var.get()
+        self.str_var.set(tmp[1:] + tmp[0])
+        self.root.after(self.interval, self.change_str)
+
+
+Marquee(tkinter.Tk(), '阿梨今天开胯了吗...   ')
